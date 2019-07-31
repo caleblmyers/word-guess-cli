eae1fa61b86bdd4009df2cbc9075ca9512e689b9
Caleb Myers <cmyers5108@gmail.com>
2019-07-31 18:48:30 -0400

    remove extra file

--- asoaif-test.js
+++
@@ -1,9 +0,0 @@
-var asoiaf = require("api-iceandfire")
-
-var randIndex = Math.floor(Math.random() * 500 + 100)
-
-asoiaf
-  .getCharacter(randIndex)
-  .then(function(res) {
-    console.log(res.name)
-  })
